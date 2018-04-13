2e4b8ca9c8220fe708bc1dd829f344e8b0834c84
quangtrangle <trang.le2@asiantech.vn>
2018-04-13 14:41:04 +0700

    select data from db and display in screen

--- connect_db.php
+++ connect_db.php
@@ -1,15 +1,17 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "trangeo22";
-  $namedb = "CRUD_basic";
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$namedb", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // return $conn;
-    echo "Connect successful!";
-  }
-  catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-  }
+  function connect(){
+		$servername = "localhost";
+		$username = "root";
+		$password = "trangeo22";
+		$namedb = "CRUD_basic";
+		try {
+			$conn = new PDO("mysql:host=$servername;dbname=$namedb", $username, $password);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $conn;
+			// echo "Connect successful!";
+		}
+		catch(PDOException $e){
+			echo "Connection failed: " . $e->getMessage();
+		}
+	}
 ?>
